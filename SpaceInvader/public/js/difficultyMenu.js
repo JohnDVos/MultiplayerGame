f4634f08cba7d9fa44c8b36d89e27afaa4b8735c
JohnDVos <47853505+JohnDVos@users.noreply.github.com>
2020-04-26 16:11:01 +0100

    John
    Added, boss & enemy assets.
    Assets spawn in random areas on menu screen
    added difficulty options
    settings hold audio options for now (no sound track added yet).

--- SpaceInvader/public/js/difficultyMenu.js
+++ SpaceInvader/public/js/difficultyMenu.js
@@ -1,10 +1,10 @@
-//Settings Menu
-class settingsMenu extends Phaser.Scene{
+//Difficulty Menu
+class difficultyMenu extends Phaser.Scene{
     constructor(){
-        super("settingsMenu"); 
+        super("difficultyMenu"); 
     }
-	
-	preload(){
+    
+    preload(){
 		//background asset:
 		this.load.image('space', 'assets/space_background.png');
 		
@@ -12,26 +12,33 @@ class settingsMenu extends Phaser.Scene{
         this.load.image('menu', 'assets/menu/menu.png');
         this.load.image('next', 'assets/menu/next.png');
         this.load.image('back', 'assets/menu/back.png');
-		
-		//sound assets:
-		this.load.image('sound_on', 'assets/menu/sound_on.png');
-		this.load.image('sound_off', 'assets/menu/sound_off.png');
-    }
 	
-	create(){
+        //dificulty assets:
+        this.load.image('easy', 'assets/menu/easy.png');
+        this.load.image('medium', 'assets/menu/medium.png');
+        this.load.image('hard', 'assets/menu/hard.png');
+    }
+    
+    create(){
         this.add.image(750,375, 'space'); 
 		
         //Settings Menu
         
-        let easyButton = this.add.image(game.config.width/2, game.config.height/2, 'sound_on').setDisplaySize(128, 64);
+        let easyButton = this.add.image(game.config.width/3, game.config.height/2, 'easy').setDisplaySize(128, 64);
         easyButton.setInteractive();
         easyButton.once('pointerdown', function(pointer){
             
         });
         
-        let mediumButton = this.add.image(game.config.width/2.5, game.config.height/2, 'sound_off').setDisplaySize(128, 64);
+        let mediumButton = this.add.image(game.config.width/2, game.config.height/2, 'medium').setDisplaySize(128, 64);
         mediumButton.setInteractive();
         mediumButton.once('pointerdown', function(pointer){
+            
+        });
+        
+        let hardButton = this.add.image(game.config.width/1.5, game.config.height/2, 'hard').setDisplaySize(128, 64);
+        hardButton.setInteractive();
+        hardButton.once('pointerdown', function(pointer){
             
         });
         
@@ -39,7 +46,7 @@ class settingsMenu extends Phaser.Scene{
         backButton.setInteractive();
         backButton.once('pointerdown', function(pointer){
             game.scene.start('mainMenu');
-            game.scene.stop('settingsMenu');
+            game.scene.stop('difficultyMenu');
         });
 
     }
--- SpaceInvader/public/js/mainMenu.js
+++ SpaceInvader/public/js/mainMenu.js
@@ -5,14 +5,29 @@ class mainMenu extends Phaser.Scene{
     }
     
     preload(){
+		//background asset:
+		this.load.image('space', 'assets/space_background.png');
+		
         //menu assets:
         this.load.image('help', 'assets/menu/help.png');
         this.load.image('settings', 'assets/menu/settings.png');
+		this.load.image('difficulty', 'assets/menu/difficulty.png');
         this.load.image('playGame', 'assets/menu/playGame.png');
+		
+		//player assets:
+		this.load.image('player1', 'assets/player/spaceship_1.png');
+		this.load.image('player2', 'assets/player/spaceship_2.png');
+		
+		//enemy assets:
+		this.load.image('enemy_1', 'assets/enemy/enemy_1.png');
+		this.load.image('enemy_2', 'assets/enemy/enemy_2.png');
+		this.load.image('enemy_3', 'assets/enemy/enemy_3.png');
+		this.load.image('boss', 'assets/enemy/boss.png');
     }
     
     create(){
-        
+        this.add.image(750,375, 'space');
+		
         //Main Menu
         
         //creates a button and sets a clause if it is pressed or not
@@ -22,12 +37,19 @@ class mainMenu extends Phaser.Scene{
             game.scene.start('mainGame');
         });
         
-        let settingsButton = this.add.image(game.config.width/2, game.config.height/2, 'settings').setDisplaySize(128, 64);
-        settingsButton.setInteractive();
-        settingsButton.once('pointerdown', function(pointer){ 
+        let difficultyButton = this.add.image(game.config.width/2, game.config.height/2.25, 'difficulty').setDisplaySize(128, 64);
+        difficultyButton.setInteractive();
+        difficultyButton.once('pointerdown', function(pointer){ 
             game.scene.stop('mainMenu');
-            game.scene.start('settingsMenu');
+            game.scene.start('difficultyMenu');
         });
+		
+		let settingsButton = this.add.image(game.config.width/2, game.config.height/1.8, 'settings').setDisplaySize(128, 64);
+		settingsButton.setInteractive();
+		settingsButton.once('pointerdown', function(pointer) {
+			game.scene.stop('mainMenu');
+			game.scene.start('settingsMenu');
+		})
         
         let helpButton = this.add.image(game.config.width/2, game.config.height/1.5, 'help').setDisplaySize(128, 64);
         helpButton.setInteractive();
@@ -35,6 +57,36 @@ class mainMenu extends Phaser.Scene{
 
             console.log("sup");
         });
-        
+		
+		//spawns player in background.
+		this.player1 = this.add.image(config.width/6, config.height/1, 'player1');
+		this.player2 = this.add.image(config.width/5 + 50, config.height/1, 'player2');
+		
+		this.enemy_1 = this.add.image(config.width/4, config.height/1, 'enemy_1');
+		this.enemy_2 = this.add.image(config.width/3, config.height/1, 'enemy_2');
+		this.enemy_3 = this.add.image(config.width/2, config.height/1, 'enemy_3');
+        this.boss = this.add.image(config.width/2.2, config.height/1, 'boss');
     }
+	
+	update() {																														//update for ships to move at different speeds.
+		this.moveShip(this.player1, 2);
+		this.moveShip(this.player2, 2);
+		
+		this.moveShip(this.enemy_1, 2);
+		this.moveShip(this.enemy_2, 2);
+		this.moveShip(this.enemy_3, 2);
+		this.moveShip(this.boss, 2);
+	}
+	
+	moveShip(ship, speed) {
+		ship.y += speed;																											//takes param for ship object & y velocity.
+		if(ship.y > config.height) {																								//if vertical position exceeds height of game.
+			this.resetShipPos(ship);																								//calls reset ship position.	
+		}
+	}
+	resetShipPos(ship) {
+		ship.y = 0;																													//takes ship object & sets Y to 0.
+		var randomX = Phaser.Math.Between(0, config.width);																			//creates random value between 0 & width of canvas.
+		ship.x = randomX;																											//assigns to x position.
+	}
 }
