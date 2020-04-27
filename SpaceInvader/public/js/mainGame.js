d853de7a659431e1b11ca5a4c9f8007b04e970f3
JohnDVos <47853505+JohnDVos@users.noreply.github.com>
2020-04-27 11:52:18 +0100

    John
    power up

--- SpaceInvader/public/js/mainGame.js
+++ SpaceInvader/public/js/mainGame.js
@@ -6,110 +6,135 @@ class mainGame extends Phaser.Scene{
     }
     
     preload() {
-        this.load.image('space', 'assets/space_background.png');														//load background image.
+        this.load.image('space', 'assets/space_background.png');																	//load background image.
 	
 	   //players:
         this.load.image('ship', 'assets/player/spaceship_1.png');
         this.load.image('ship2', 'assets//player/spaceship_2.png');
 	
 	   //power-ups:
-	   this.load.image('heartPowerUp', 'assets/power-ups/heartPowerUp.png');											//loads health power-up asset.
-	   this.load.image('damagePowerUp', 'assets/power-ups/damagePowerUp.png');											//loads damange power-up asset.
+	   this.load.image('heartPowerUp', 'assets/power-ups/heartPowerUp.png');														//loads health power-up asset.
+	   this.load.image('damagePowerUp', 'assets/power-ups/damagePowerUp.png');														//loads damange power-up asset.
 	
     }
  
     create() {
-    var self = this;
-    this.add.image(750,375, 'space');
-    this.socket = io();
-    this.otherPlayers = this.physics.add.group();
-    this.socket.on('currentPlayers', function (players) {
-        Object.keys(players).forEach(function (id) {
-            if (players[id].playerId === self.socket.id) {
-                self.addPlayer(self, players[id]);
-            } else {
-                self.addOtherPlayers(self, players[id]);
-            }
-        });
-    });
-    this.socket.on('newPlayer', function (playerInfo) {
-        self.addOtherPlayers(self, playerInfo);
-    });
-    this.socket.on('disconnect', function (playerId) {
-        self.otherPlayers.getChildren().forEach(function (otherPlayer) {
-        if (playerId === otherPlayer.playerId) {
-            otherPlayer.destroy();
-            }
-        });
-    });
-    	
-    this.cursors = this.input.keyboard.createCursorKeys();
-    
-    this.socket.on('playerMoved', function (playerInfo) {
-        self.otherPlayers.getChildren().forEach(function (otherPlayer) {
-            if (playerInfo.playerId === otherPlayer.playerId) {
-                otherPlayer.setRotation(playerInfo.rotation);
-                otherPlayer.setPosition(playerInfo.x, playerInfo.y);
-            }
-        });
-    });
-}
+		var self = this;
+		this.add.image(750,375, 'space');
+		this.socket = io();
+		this.otherPlayers = this.physics.add.group();
+		this.socket.on('currentPlayers', function (players) {
+			Object.keys(players).forEach(function (id) {
+				if (players[id].playerId === self.socket.id) {
+					self.addPlayer(self, players[id]);
+				} else {
+					self.addOtherPlayers(self, players[id]);
+				}
+			});
+		});
+		this.socket.on('newPlayer', function (playerInfo) {
+			self.addOtherPlayers(self, playerInfo);
+		});
+		this.socket.on('disconnect', function (playerId) {
+			self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+			if (playerId === otherPlayer.playerId) {
+				otherPlayer.destroy();
+				}
+			});
+		});
+
+		this.cursors = this.input.keyboard.createCursorKeys();
+
+		this.socket.on('playerMoved', function (playerInfo) {
+			self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+				if (playerInfo.playerId === otherPlayer.playerId) {
+					otherPlayer.setRotation(playerInfo.rotation);
+					otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+				}
+			});
+		});
+		
+		//adds text game object to display team scores:
+		this.redScoreText = this.add.text(1000, 16, '', { fontSize: '32px', fill: '#FF0000' });											//red team score.
+		this.blueScoreText = this.add.text(16, 16, '', { fontSize: '32px', fill: '#0000FF' });											//blue team score.
+		
+		this.socket.on('scoreUpdate', function(scores) {																				//when score event received.
+			self.redScoreText.setText('Red team score: ' + scores.blue);																//passes score.
+			self.blueScoreText.setText('Blue team score: ' + scores.blue);																//passes score.
+		});
+		
+		this.socket.on('heartPowerUpLocation', function(heartPowerUpLocation) {
+			if(self.heartPowerUp) self.heartPowerUp.destroy();
+			self.heartPowerUp = self.physics.add.image(heartPowerUpLocation.x, heartPowerUpLocation.y, 'heartPowerUpLocation');			//add new heart power-up object to players game.
+			self.physics.add.overlap(self.ship, self.heartPowerUpLocation, function() {													//check if player's ship & power-up overlap.
+				this.socket.emit('heartPowerUpCollected');
+			}, null, self);
+		});
+		
+		this.socket.on('damagePowerUpLocation', function(damagePowerUpLocation) {
+			if(self.damagePowerUp) self.damagePowerUp.destroy();
+			self.damagePowerUp = self.physics.add.image(damagePowerUpLocation.x, damagePowerUpLocation.y, 'damagePowerUpLocation');		//add new heart power-up object to players game.
+			self.physics.add.overlap(self.ship, self.damagePowerUpLocation, function() {													//check if player's ship & power-up overlap.
+				this.socket.emit('damagePowerUpCollected');
+			}, null, self);
+		});
+	}
  
     update() {
-    if (this.ship) {
-    if (this.cursors.left.isDown) {
-        this.ship.setAngularVelocity(-150);
-    }   else if (this.cursors.right.isDown) {
-        this.ship.setAngularVelocity(150);
-    }   else {
-        this.ship.setAngularVelocity(0);
-    }
-  
-    if (this.cursors.up.isDown) {
-        this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
-    }   else {
-        this.ship.setAcceleration(0);
-    }
-  
-    //this.physics.world.wrap(this.ship, 5);
-        
-    // emit player movement
-    var x = this.ship.x;
-    var y = this.ship.y;
-    var r = this.ship.rotation;
-    if (this.ship.oldPosition && (x !== this.ship.oldPosition.x || y !== this.ship.oldPosition.y || r !== this.ship.oldPosition.rotation)) {
-    this.socket.emit('playerMovement', { x: this.ship.x, y: this.ship.y, rotation: this.ship.rotation });
-    }
- 
-    // save old position data
-    this.ship.oldPosition = {
-        x: this.ship.x,
-        y: this.ship.y,
-        rotation: this.ship.rotation
-    };
-  }
-}
+		if (this.ship) {
+		if (this.cursors.left.isDown) {
+			this.ship.setAngularVelocity(-150);
+		}   else if (this.cursors.right.isDown) {
+			this.ship.setAngularVelocity(150);
+		}   else {
+			this.ship.setAngularVelocity(0);
+		}
+
+		if (this.cursors.up.isDown) {
+			this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
+		}   else {
+			this.ship.setAcceleration(0);
+		}
+
+		//this.physics.world.wrap(this.ship, 5);
+
+		// emit player movement
+		var x = this.ship.x;
+		var y = this.ship.y;
+		var r = this.ship.rotation;
+		if (this.ship.oldPosition && (x !== this.ship.oldPosition.x || y !== this.ship.oldPosition.y || r !== this.ship.oldPosition.rotation)) {
+		this.socket.emit('playerMovement', { x: this.ship.x, y: this.ship.y, rotation: this.ship.rotation });
+		}
+
+		// save old position data
+		this.ship.oldPosition = {
+			x: this.ship.x,
+			y: this.ship.y,
+			rotation: this.ship.rotation
+		};
+	  }
+	}
 
     addPlayer(self, playerInfo) {
-  self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
-  //if (playerInfo.team === 'blue') {
-  //  self.ship.setTint(0x0000ff);
-  //} else {
-  //  self.ship.setTint(0xff0000);
-  //}
-  self.ship.setDrag(100);
-  self.ship.setAngularDrag(100);
-  self.ship.setMaxVelocity(200);
-}
+	  self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+	  //if (playerInfo.team === 'blue') {
+	  //  self.ship.setTint(0x0000ff);
+	  //} else {
+	  //  self.ship.setTint(0xff0000);
+	  //}
+	  self.ship.setDrag(100);
+	  self.ship.setAngularDrag(100);
+	  self.ship.setMaxVelocity(200);
+	}
 
     addOtherPlayers(self, playerInfo) {
-  const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'ship2').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
-  //if (playerInfo.team === 'blue') {
-  //  otherPlayer.setTint(0x0000ff);
-  //} else {
-  //  otherPlayer.setTint(0xff0000);
-  //}
-  otherPlayer.playerId = playerInfo.playerId;
-  self.otherPlayers.add(otherPlayer);
-}
+	  const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'ship2').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+	  //if (playerInfo.team === 'blue') {
+	  //  otherPlayer.setTint(0x0000ff);
+	  //} else {
+	  //  otherPlayer.setTint(0xff0000);
+	  //}
+	  otherPlayer.playerId = playerInfo.playerId;
+	  self.otherPlayers.add(otherPlayer);
+	}
 }
--- SpaceInvader/server.js
+++ SpaceInvader/server.js
@@ -21,10 +21,29 @@ io.on('connection', function (socket) {
         playerId: socket.id,
         team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
     };
-    // send the players object to the new player
-    socket.emit('currentPlayers', players);
-    // update all other players of the new player
-    socket.broadcast.emit('newPlayer', players[socket.id]);  
+	
+	var heartPowerUp = {
+		x : Math.floor(Math.random() * 700) + 50,
+		y : Math.floor(Math.random() * 500) + 50
+	};
+	
+	var damagePowerUp = {
+		x : Math.floor(Math.random() * 700) + 50,
+		y : Math.floor(Math.random() * 500) + 50
+	};
+	
+	var scores = {
+		blue : 0,
+		red : 0
+	};
+	
+    
+    socket.emit('currentPlayers', players);																				// send the players object to the new player.
+    socket.broadcast.emit('newPlayer', players[socket.id]);  															// update all other players of the new player.
+	
+	socket.emit('heartPowerUpLocation', heartPowerUp);																	//send heart power-up object to the new player.
+	socket.emit('damagePowerUpLocation', damagePowerUp);																//send damage power-up object to the new player.
+	socket.emit('scoreUpdate', scores);																					//send the current scores.
     
     socket.on('disconnect', function () {
         console.log('user disconnected');
@@ -41,6 +60,30 @@ io.on('connection', function (socket) {
     // emit a message to all players about the player that moved
         socket.broadcast.emit('playerMoved', players[socket.id]);
     });
+	
+	socket.on('heartPowerUpCollected', function() {																			//when a heart power-up is collected.
+		if(players[socket.id].team === 'red') {																				//if the socket id belongs to the red team.
+			scores.red += 10;																								//red teams score + 10 points.
+		} else {																											//if the socket id belongs to the blue team.
+			scores.blue += 10;																								//blue teams score + 10 points.
+		}
+		heartPowerUp.x = Math.floor(Math.random() * 700) + 50;																//x co-ordinate.
+		heartPowerUp.y = Math.floor(Math.random() * 500) + 50;																//u co-ordinate.
+		io.emit('heartPowerUpLocation', heartPowerUp);																		//emits heart power-up was picked up.
+		io.emit('scoreUpdate', scores);																						//updates score.
+	});
+	
+	socket.on('damagePowerUpCollected', function() {																		//when a damage power-up is collected.
+		if(players[socket.id].team === 'red') {																				//if the socket id belongs to the red team.
+			scores.red += 10;																								//red teams score + 10 points.
+		} else {																											//if the socket id belongs to the blue team.
+			scores.blue += 10;																								//blue teams score + 10 points.
+		}
+		damagePowerUp.x = Math.floor(Math.random() * 700) + 50;																//x co-ordinate.
+		damagePowerUp.y = Math.floor(Math.random() * 500) + 50;																//u co-ordinate.
+		io.emit('damagePowerUpLocation', damagePowerUp);																	//emits damage power-up was picked up.
+		io.emit('scoreUpdate', scores);																						//updates score.
+	});
 });
 
 server.listen(8081, function () {
